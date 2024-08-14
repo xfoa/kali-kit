fe03024a860dbb6bcbf876fac3fde211174477bb
foax <a@fo.ax>
2024-08-15 01:28:01 +0300

    Write ISO build script

--- bin/make-iso.sh
+++ bin/make-iso.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-readonly REPO_ROOT="$(git rev-parse --toplevel)"
+set -eu
 
-echo "$REPO_ROOT"
+readonly REPO_ROOT="$(git rev-parse --show-toplevel)"
+pushd "$REPO_ROOT"
+rm build/kali_kit.iso 2>/dev/null || true
+xorriso -outdev build/kali_kit.iso -joliet off -map iso / -volid KALI_KIT
+popd
