2faffa5a7b5068ab8d5bc7c71c21f21fbf6f38f5
foax <a@fo.ax>
2024-08-15 20:05:44 +0300

    Fix docs text output formatting

--- make-docs.sh
+++ make-docs.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 set -eu
 
-readonly DOCS_DIR='html_docs'
+readonly HTML_DOCS_DIR='build/html_docs'
+readonly TXT_DOCS_DIR='src/iso/DOCS'
 
-rm -r "build/${DOCS_DIR:?Output dir wasn\'t set!}" 2> /dev/null || true
-mkdir -p "build/$DOCS_DIR"
+rm -r "${HTML_DOCS_DIR:?HTML output dir wasn\'t set!}" 2> /dev/null || true
+mkdir -p "$HTML_DOCS_DIR"
 
+rm -r "${TXT_DOCS_DIR:?TXT output dir wasn\'t set!}" 2> /dev/null || true
+mkdir -p "$TXT_DOCS_DIR"
+
+echo "Writing HTML docs to $HTML_DOCS_DIR"
+echo "Writing TXT docs to $TXT_DOCS_DIR, which will be included in the ISO next time it's built"
 for doc in src/docs/*.md
 do
-	pandoc -f markdown -t html --template=lib/bootstrap_menu.html -o "build/$DOCS_DIR/$(basename ${doc/%.md/.html})" -s --lua-filter lib/links-to-html.lua "$doc" 
+	pandoc -f markdown -t html --template=lib/bootstrap_menu.html -o "$HTML_DOCS_DIR/$(basename ${doc/%.md/.html})" -s --lua-filter lib/links-to-html.lua "$doc" 
+	pandoc -f markdown -t rst --columns 80 -o "$TXT_DOCS_DIR/$(basename ${doc/%.md/.txt})" -s --lua-filter lib/links-to-txt.lua "$doc" 
 done
+unix2dos "$TXT_DOCS_DIR"/*.txt
--- src/docs/install.md
+++ src/docs/install.md
@@ -4,3 +4,8 @@ title: foax's Kali Kit Documentation - Install
 
 # Installing network drivers and Kali in DOS
 
+This is a very long sentence that should overflow the 80 character limit if I structure it carefully.
+This is another sentence in the same paragraph that I should be able to write into a very long sentence too, easily overflowing the 80 character limit.
+
+Some more text because I don't have a copy of Lorem ipsum to hand, otherwise I would have used that.
+As it is, I just have to write some text myself that is very long so that I can use it as test data for generating docs.
